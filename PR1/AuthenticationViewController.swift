9021eaa5a522ae76206511008e78bba9c436b24a
dmorenoar <dmorenoar@192.168.170.128>
2019-02-28 14:10:04 +0100

    PAC1 - Fase 6

--- PR1/AuthenticationViewController.swift
+++ PR1/AuthenticationViewController.swift
@@ -10,9 +10,60 @@ import UIKit
 class AuthenticationViewController: UIViewController, UITextFieldDelegate {
     // BEGIN-UOC-4
 
+    //Definición de los outlets de los fields
+    @IBOutlet weak var firstField: UITextField!
+    @IBOutlet weak var secondField: UITextField!
+    @IBOutlet weak var thirdField: UITextField!
+    @IBOutlet weak var fourthField: UITextField!
+    
+    //Array que contiene los IBOutlet de los UITextField
+    var arrayFields:[UITextField] = [UITextField]()
+    
+    @IBAction func nextButtonTapped(_ sender: UIButton) {
+        var code:String = ""
+        
+        /*Concatenamos los valores de los fields recorriendo el array que los contiene para generar el código introducido por el usuario*/
+        for field in arrayFields{
+            code += field.text ?? "\u{00a0}"
+        }
+        
+        /*Realizamos la validación del código usando el método validate*/
+        if Services.validate(code: code){
+            performSegue(withIdentifier: "SegueToMainNavigation", sender: self)
+        }else{
+            Utils.show(Message: "Sorry, the entered code is not valid", WithTitle: "Code Error", InViewController: self)
+        }
+        
+    }
+    
+    /*Definimos la función viewDidLoad para que en la carga de la view podamos
+     asignar a cada field cómo delegado la propia view y así gestionar su comportamiento*/
+    override func viewDidLoad() {
+        
+        firstField.delegate = self
+        secondField.delegate = self
+        thirdField.delegate = self
+        fourthField.delegate = self
+        
+        /*Añadimos los fields a un array para gestionar de una forma más cómoda*/
+        arrayFields.append(firstField)
+        arrayFields.append(secondField)
+        arrayFields.append(thirdField)
+        arrayFields.append(fourthField)
+        
+    }
+    
+    /*Método heredado de UITextFieldDelegate que nos permite gestionar que el usuario únicamente pueda introducir dígitos numéricos en los inputFields */
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        return Int(string) != nil
+    }
+    
+    
     // END-UOC-4
     
     @IBAction func backButtonTapped(_ sender: UIButton) {
         dismiss(animated: true, completion: nil)
-    } 
+    }
+    
+    
 }
--- PR1/LoginViewController.swift
+++ PR1/LoginViewController.swift
@@ -25,11 +25,37 @@ class LoginViewController: UIViewController {
     // END-UOC-2
     
     // BEGIN-UOC-5
-
+    /*Controlamos en el viewWillAppear que si el usuario vuelve a la vista de login, tanto usuario como password no se le muestre evitando fallos de seguridad. Cómo la vista ya esta creada y su viewDidLoad se ha ejecutado y creado la vista, la siguiente fase en el ciclo de vida será el viewWillAppear*/
+    override func viewWillAppear(_ animated: Bool) {
+        usernameField.text = ""
+        passwordField.text = ""
+    }
     // END-UOC-5
     
     // BEGIN-UOC-6
+    /*En la carga inicial de la view, controlamos que según la hora se muestre un mensaje personalizado y color fondo de pantalla determinado*/
+    override func viewDidLoad() {
+        
+        let hour = Calendar.current.component(.hour, from: Date())
+        print(hour)
+        
+        if hour >= 6 && hour <= 11{
+            welcomeLabel.text = "Good morning"
+            self.view.backgroundColor = UIColor.orange.lighter()
+        }else if hour >= 12 && hour <= 17 {
+            welcomeLabel.text = "Good afternoon"
+            self.view.backgroundColor = UIColor.yellow.lighter()
+        }else if hour >= 18 && hour <= 21 {
+            welcomeLabel.text = "Good evening"
+            self.view.backgroundColor = UIColor.red.lighter()
+        }else {
+            welcomeLabel.text = "Good night"
+            self.view.backgroundColor = UIColor.blue.lighter()
+        }
 
+    }
+    
+    
     // END-UOC-6
     
     @IBAction func unwindToLogin(for unwindSegue: UIStoryboardSegue, towardsViewController subsequentVC: UIViewController) {
